8829866b3ec5afe4ba78e66c3cf01056c4565690
halfak <aaron.halfaker@gmail.com>
2015-09-25 00:38:57 -0500

    Adds main utilities and process_args functions to __init__ in utilities.

--- mwdiffs/utilities/__init__.py
+++ mwdiffs/utilities/__init__.py
@@ -1,4 +1,5 @@
-from .dump2diffs import dump2diffs
-from .revdocs2diffs import revdocs2diffs
+from .dump2diffs import dump2diffs, process_args as dump2diffs_args, drop_text
+from .revdocs2diffs import revdocs2diffs, process_args as revdocs2diffs_args
 
-__all__ = [dump2diffs, revdocs2diffs]
+__all__ = [dump2diffs, revdocs2diffs, dump2diffs_args, revdocs2diffs_args,
+           drop_text]
